3b3da7fc89e0f1a9f0e31ac07979651f9d0f61a5
Romi Than <rrromithan@gmail.com>
2024-04-26 15:28:58 +0700

    m

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   basePath: "/js-calculator-nextjs",
   output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
